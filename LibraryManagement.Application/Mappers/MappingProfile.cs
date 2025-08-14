cf97b5c6bbbbd5f447dcb459aff21e70afdcb7a3
Henry García <henrygarciaospina@gmail.com>
2025-08-14 04:29:42 -0500

    Configuración de los mappers.

---
+++ LibraryManagement.Application/Mappers/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using LibraryManagement.Core.Entities;
+using LibraryManagement.Application.DTOs;
+
+namespace LibraryManagement.Application.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Books
+            CreateMap<Book, BookDto>().ReverseMap();
+            CreateMap<BookCreateDto, Book>();
+            CreateMap<BookUpdateDto, Book>();
+
+            // Members
+            CreateMap<Member, MemberDto>().ReverseMap();
+            CreateMap<MemberCreateDto, Member>();
+            CreateMap<MemberUpdateDto, Member>();
+
+            // Loans
+            CreateMap<Loan, LoanDto>().ReverseMap();
+            CreateMap<LoanCreateDto, Loan>();
+            CreateMap<LoanUpdateDto, Loan>();
+
+            // Libraries
+            CreateMap<Library, LibraryDto>().ReverseMap();
+            CreateMap<LibraryCreateDto, Library>();
+            CreateMap<LibraryUpdateDto, Library>();
+        }
+    }
+}
